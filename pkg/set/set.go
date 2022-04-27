50f07353165dcef6af56a0370d8ebce1de8e9c3e
Jason Friedland <jason@friedland.id.au>
2022-04-27 13:18:04 +1000

    Struct test, doc comments

--- pkg/set/set.go
+++ pkg/set/set.go
@@ -1,13 +1,17 @@
 package set
 
+// Set is a non-thread safe generic type to hold a slice of un-ordered unique
+// `comparable` items.
 type Set[T comparable] struct {
 	items []T
 }
 
+// New builds a new Set of type `T`.
 func New[T comparable]() *Set[T] {
 	return &Set[T]{}
 }
 
+// Add appends a new item to the Set if it doesn't already exist.
 func (s *Set[T]) Add(v T) {
 	if _, found := s.In(v); found {
 		return
@@ -15,13 +19,19 @@ func (s *Set[T]) Add(v T) {
 	s.items = append(s.items, v)
 }
 
+// Del removes an item by value. The deletion algorithm DOES NOT retain the
+// order of items.
 func (s *Set[T]) Del(v T) {
 	if i, found := s.In(v); found {
+		// Replace the item at index `i` with the last item of the
+		// slice, and discard the last item
 		s.items[i] = s.items[len(s.items)-1]
 		s.items = s.items[:len(s.items)-1]
 	}
 }
 
+// In returns the index and a bool indicating the position and existence of an
+// item by value.
 func (s *Set[T]) In(v T) (int, bool) {
 	for i, k := range s.items {
 		if k == v {
@@ -31,6 +41,7 @@ func (s *Set[T]) In(v T) (int, bool) {
 	return -1, false
 }
 
+// Len returns the length of the Set.
 func (s *Set[T]) Len() int {
 	return len(s.items)
 }
--- pkg/set/set_test.go
+++ pkg/set/set_test.go
@@ -7,6 +7,7 @@ import (
 )
 
 func TestSet(t *testing.T) {
+	// String test
 	t.Run("Set, string", func(t *testing.T) {
 		s := New[string]()
 
@@ -22,7 +23,7 @@ func TestSet(t *testing.T) {
 		s.Del("hello")
 		assert.Equal(t, s.items, []string{"bar", "foo"})
 	})
-
+	// Int test
 	t.Run("Set, int", func(t *testing.T) {
 		s := New[int]()
 
@@ -37,6 +38,35 @@ func TestSet(t *testing.T) {
 
 		s.Del(1234)
 		assert.Equal(t, s.items, []int{9101, 5678})
+	})
+	// Struct test
+	t.Run("Set, struct", func(t *testing.T) {
+		type tmp struct {
+			s string
+			f float64
+			b bool
+		}
+		s := New[tmp]()
 
+		s.Add(tmp{"hello", 12.0, true})
+		s.Add(tmp{"hello", 12.0, true})
+		assert.Equal(t, s.Len(), 1)
+		assert.Equal(t, s.items, []tmp{
+			{"hello", 12.0, true},
+		})
+
+		s.Add(tmp{"goodbye", 3.14159, false})
+		s.Add(tmp{"hello", 12.0, false})
+		assert.Equal(t, s.items, []tmp{
+			{"hello", 12.0, true},
+			{"goodbye", 3.14159, false},
+			{"hello", 12.0, false},
+		})
+
+		s.Del(tmp{"hello", 12.0, true})
+		assert.Equal(t, s.items, []tmp{
+			{"hello", 12.0, false},
+			{"goodbye", 3.14159, false},
+		})
 	})
 }
