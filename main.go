d0fc877bf149f2edfbbfb1c94214ed788f7ef41e
Jason Friedland <jason@friedland.id.au>
2022-04-28 15:09:41 +1000

    Add struct example in main

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
+	// Strings
 	s1 := set.New[string]()
 	s1.Add("hello")
 	s1.Add("hello")
@@ -20,6 +21,7 @@ func main() {
 	fmt.Println(s1.In("bar"))
 	fmt.Println(s1.Len())
 
+	// Ints
 	s2 := set.New[int]()
 	s2.Add(1)
 	s2.Add(43)
@@ -33,4 +35,18 @@ func main() {
 	fmt.Println(s2.In(43))
 	fmt.Println(s2.In(89))
 	fmt.Println(s2.Len())
+
+	// Structs of comparable types
+	type tmp struct {
+		s string
+		f float64
+		b bool
+	}
+	s3 := set.New[tmp]()
+	s3.Add(tmp{"hello", 12.0, true})
+	s3.Add(tmp{"hello", 12.0, true})
+	s3.Add(tmp{"foo", 3.14, false})
+	fmt.Println(s3.In(tmp{"hello", 12.0, true}))
+	s3.Del(tmp{"foo", 3.14, false})
+	fmt.Println(s3.Len())
 }
